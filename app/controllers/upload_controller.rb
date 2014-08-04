c2892dcb93b209ed971afb10e540d1e5d6f1ac10
jsrn <james.srn@gmail.com>
2014-08-04 21:03:30 +0100

    show alert for no file selected

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -2,6 +2,13 @@ include CollageBuilder
 
 class UploadController < ApplicationController
   def process_zip
+    if params[:zipfile].nil?
+      redirect_to "/", :flash => {
+        :no_file_selected => true
+      }
+      return
+    end
+
     if params[:zipfile].content_type != "application/zip"
       redirect_to "/", :flash => {
         :upload_failed => true
